64778df8a35a59173d3317dae471997762061002
jorgeja <mrjackwitz@gmail.com>
2018-02-27 21:05:24 +0100

    initial commit

---
+++ project/uart1.h
@@ -0,0 +1,17 @@
+/*
+ * uart.h
+ *
+ * Created: 18/03/17 13:13:39
+ *  Author: Sondre
+ */ 
+
+
+#ifndef UART_H_
+#define UART_H_
+#include <stdio.h>
+
+void uart_init();
+void uart_tx_char(char c,FILE* stream);
+
+
+#endif /* UART_H_ */
